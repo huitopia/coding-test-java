5d72d548eface59a832457e86611a8cdd1e84bd8
huitopia <huitopia27@gmail.com>
2024-02-22 18:00:41 +0900

    사칙연산

---
+++ src/programmers/introduction/두_수의_차.java
@@ -0,0 +1,10 @@
+package programmers.introduction;
+
+public class 두_수의_차 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            int result = num1 - num2;
+            return result;
+        }
+    }
+}
---
+++ src/programmers/introduction/몫_구하기.java
@@ -0,0 +1,10 @@
+package programmers.introduction;
+
+public class 몫_구하기 {
+    class Solution {
+        public int solution(int num1, int num2) {
+            int answer = num1/num2;
+            return answer;
+        }
+    }
+}
