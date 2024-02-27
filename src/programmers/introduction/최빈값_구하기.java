99a5cb24e65ee374a2b871b0eee585e1c1f21a01
huitopia <huitopia27@gmail.com>
2024-02-27 17:43:56 +0900

    Runtime Error

--- src/programmers/introduction/최빈값_구하기.java
+++ src/programmers/introduction/최빈값_구하기.java
@@ -1,8 +1,5 @@
 package programmers.introduction;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class 최빈값_구하기 {
     public static void main(String[] args) {
 //        최빈값 : 주어진 값 중에서 가장 자주 나오는 값
@@ -10,31 +7,6 @@ public class 최빈값_구하기 {
         int[] array = {1,2,3,3,3,4};
 //        int[] array = {1,1,2,2};
 //        int[] array = {1};
-        int[] test = new int[array.length];
-        int cnt = 0;
-        int answer = 0;
-        if(array.length == 1) {
-            answer = array[0];
-        } else {
-
-            for(int i = 0; i < array.length; i++) {
-                for(int j = 0; j < i; j++) {
-                    if(array[i] == array[j]) {
-                        test[cnt] = array[i];
-                        cnt++;
-                    }
-                }
-            }
-
-//            if(cnt == 2){
-//                answer = -1;
-//            }else {
-//                answer = test[0];
-//            }
-            answer = cnt == 2 ? -1 : test[0];
-
-        }
-        System.out.println("answer = " + answer);
 
     }
 }
