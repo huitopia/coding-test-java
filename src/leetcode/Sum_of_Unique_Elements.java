62b0363dc7bb22dfd61baed201317edee08bde48
huitopia <huitopia27@gmail.com>
2024-03-23 01:38:21 +0900

    HashSet, stream().reduce()

---
+++ src/leetcode/Sum_of_Unique_Elements.java
@@ -0,0 +1,30 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Sum_of_Unique_Elements {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,2};
+        Set<Integer> set1 = new HashSet<>();
+        Set<Integer> set2 = new HashSet<>();
+        for(int n : nums) {
+            boolean bool = set1.add(n);
+            if(!bool) {
+                set2.add(n);
+            }
+        }
+        System.out.println("set1 = " + set1);
+        System.out.println("set2 = " + set2);
+        for(int n : set2) {
+            set1.remove(n);
+        }
+        System.out.println("set1 = " + set1);
+
+        Integer result = set1.stream().reduce(0, (a, b) -> a + b);
+        System.out.println("result = " + result);
+
+    }
+}
