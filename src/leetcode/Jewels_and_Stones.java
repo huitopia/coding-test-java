86ecf22020c9d6aaace241382ee9ec33001a4982
huitopia <huitopia27@gmail.com>
2024-04-01 16:19:49 +0900

    Char -> String

---
+++ src/leetcode/Jewels_and_Stones.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Objects;
+
+public class Jewels_and_Stones {
+    public static void main(String[] args) {
+        String jewels = "aA";
+        String stones = "aAAbbbb";
+        boolean bool;
+        char c = ' ';
+        String str = "";
+        int cnt = 0;
+        for (int i = 0; i < stones.length(); i++) {
+            c = stones.charAt(i);
+            str = Character.toString(c);
+            bool = jewels.contains(str);
+            if(bool) {
+                cnt++;
+            }
+        }
+        System.out.println("cnt = " + cnt);
+    }
+}
