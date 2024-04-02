058ccbf3c336b58cb6d7d04ac398c5c5e4a2aa2c
huitopia <huitopia27@gmail.com>
2024-04-02 22:49:26 +0900

    replaceAll, 정규식, charAt

---
+++ src/programmers/introduction/숨어있는_숫자의_덧셈_1.java
@@ -0,0 +1,17 @@
+package programmers.introduction;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class 숨어있는_숫자의_덧셈_1 {
+    public static void main(String[] args) {
+        String my_string = "aAb1B2cC34oOp";
+        int answer = 0; // 10
+        String str = my_string.replaceAll("[^0-9]","");
+        for (int i = 0; i < str.length(); i++) {
+            answer += str.charAt(i) -'0';
+        }
+        System.out.println("answer = " + answer);
+    }
+}
