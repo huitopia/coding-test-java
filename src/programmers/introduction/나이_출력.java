e5f9c12b96bd76688a0ea27f1e25514201fbe6d9
huitopia <huitopia27@gmail.com>
2024-04-02 12:09:13 +0900

    int

---
+++ src/programmers/introduction/나이_출력.java
@@ -0,0 +1,10 @@
+package programmers.introduction;
+
+public class 나이_출력 {
+    public static void main(String[] args) {
+        int age = 40;
+        // 2022 년 기준
+        // 2024년 = 31살 = 1994
+        System.out.println(2022 - age + 1);
+    }
+}
