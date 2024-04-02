5d3046fe83a4c44c48489a98d098b1e0829ab96c
huitopia <huitopia27@gmail.com>
2024-04-02 14:07:00 +0900

    sum

--- src/programmers/introduction/양꼬치.java
+++ src/programmers/introduction/양꼬치.java
@@ -5,11 +5,8 @@ public class 양꼬치 {
         int n = 10;
         int k = 3;
         // result = 124000
-        int drink = 2000;
-        int food = 12000;
         int serviceCount = n / 10;
-        int serviceDrinkPrice =serviceCount * drink;
-        int sum =( n * food )+ (k * drink);
-        System.out.println(sum - serviceDrinkPrice);
+        int result = (12000*n) + (2000*(k - serviceCount));
+        System.out.println("result = " + result);
     }
 }
