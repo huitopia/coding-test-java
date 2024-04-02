756908f1b590c180ad2682bbb7cebe4399ad923c
huitopia <huitopia27@gmail.com>
2024-04-02 17:43:10 +0900

    문자열 길이

---
+++ src/programmers/introduction/편지.java
@@ -0,0 +1,8 @@
+package programmers.introduction;
+
+public class 편지 {
+    public static void main(String[] args) {
+        String message = "happy birthday!";
+        int answer = message.length() * 2;
+    }
+}
