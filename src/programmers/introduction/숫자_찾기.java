6bc52e0df8fce52ae292ff3ff8e7979714830b83
huitopia <huitopia27@gmail.com>
2024-04-04 16:45:10 +0900

    indexOf 활용

--- src/programmers/introduction/숫자_찾기.java
+++ src/programmers/introduction/숫자_찾기.java
@@ -5,14 +5,8 @@ public class 숫자_찾기 {
     public static void main(String[] args) {
         int num = 232443;
         int k = 4;
-        int answer = -1; // num 의 k 값 index 3 i + 1 없으면 -1
         String str = String.valueOf(num);
-        for (int i = 0; i < str.length(); i++) {
-            if(Integer.toString(k).charAt(0) == str.charAt(i)) {
-                    answer = i + 1;
-                    break;
-            }
-        }
+        int answer = str.contains(String.valueOf(k)) ? str.indexOf(String.valueOf(k)) + 1 : -1;
         System.out.println("answer = " + answer);
 
     }
