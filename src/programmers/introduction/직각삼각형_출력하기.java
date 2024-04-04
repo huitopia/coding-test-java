6774ba28da82d38beae3f4d9ae57a8f780dbce36
huitopia <huitopia27@gmail.com>
2024-04-04 14:06:20 +0900

    for 문 별 찍기

---
+++ src/programmers/introduction/직각삼각형_출력하기.java
@@ -0,0 +1,12 @@
+package programmers.introduction;
+
+public class 직각삼각형_출력하기 {
+    public static void main(String[] args) {
+        int n = 3;
+        String star = "";
+        for (int i = 0; i < n; i++) {
+            star += "*";
+            System.out.println(star);
+        }
+    }
+}
